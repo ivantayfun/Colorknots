1993fe3c8fa224c1c950f81a8806c9efbd0ccaf0
Ivan Rybakov <ivan.rubakov85@gmail.com>
2019-01-20 22:45:46 +0200

    20.01.2019

--- app/src/main/java/com/example/ivan/colorknots/CenacosRound.java
+++ app/src/main/java/com/example/ivan/colorknots/CenacosRound.java
@@ -3,7 +3,7 @@ package com.example.ivan.colorknots;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+//import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,13 +20,17 @@ public class CenacosRound extends AppCompatActivity {
     EditText editText_shirina_ovala;
     EditText editText_skidka;
     SharedPreferences sp;
-    String Parser_DATA;
+    //String Parser_DATA;
     String cena_blije_k_vbIbr_kose;
+    //String ggg1;
 
     int dlina_blije_k_vbIbr_kose;
     double dlina_cosu;
     double fff;
     double skidka;
+    double chislo;
+    double osnovanie;
+    double ccc;
     double skidka_rezylbtat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,84 +61,25 @@ public class CenacosRound extends AppCompatActivity {
                     dlina_cosu = Integer.parseInt(editText_shirina_ovala.getText().toString()) * 3.14 + (Integer.parseInt(editText_dlina_ovala.getText().toString())-Integer.parseInt(editText_shirina_ovala.getText().toString())) * 2;
                 }
 
-                if (Integer.parseInt(editText_trybok_v_kose.getText().toString())==3){
-                    if (dlina_cosu==120){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_3_120");
-                        dlina_blije_k_vbIbr_kose = 120;
-                    }else if (dlina_cosu>=120&dlina_cosu<180){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_3_120");
-                        dlina_blije_k_vbIbr_kose = 120;
-                    }else if (dlina_cosu>=180&dlina_cosu<240){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_3_180");
-                        dlina_blije_k_vbIbr_kose = 180;
-                    }else if (dlina_cosu>=240&dlina_cosu<360){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_3_240");
-                        dlina_blije_k_vbIbr_kose = 240;
-                    }else if (dlina_cosu>=360){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_3_360");
-                        dlina_blije_k_vbIbr_kose = 360;
-                    }
 
-                }
-
-                else if (Integer.parseInt(editText_trybok_v_kose.getText().toString())==4){
-                    if (dlina_cosu<120){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_4_120");
-                        dlina_blije_k_vbIbr_kose = 120;
-                    }
-                    if (dlina_cosu>=120&dlina_cosu<180){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_4_120");
-                        dlina_blije_k_vbIbr_kose = 120;
-                    }
-                    if (dlina_cosu>=180&dlina_cosu<240){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_4_180");
-                        dlina_blije_k_vbIbr_kose = 180;
-                    }
-                    if (dlina_cosu>=240&dlina_cosu<360){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_4_240");
-                        dlina_blije_k_vbIbr_kose = 240;
-                    }
-                    if (dlina_cosu>=360){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_4_360");
-                        dlina_blije_k_vbIbr_kose = 360;
-                    }
-
-                }
-                else if (Integer.parseInt(editText_trybok_v_kose.getText().toString())>4){
-                    if (dlina_cosu<120){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_4_120");
-                        dlina_blije_k_vbIbr_kose = 120*4/Integer.parseInt(editText_trybok_v_kose.getText().toString());
-                    }
-                    if (dlina_cosu>=120&dlina_cosu<180){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_4_120");
-                        dlina_blije_k_vbIbr_kose = 120*4/Integer.parseInt(editText_trybok_v_kose.getText().toString());
-                    }
-                    if (dlina_cosu>=180&dlina_cosu<240){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_4_180");
-                        dlina_blije_k_vbIbr_kose = 180*4/Integer.parseInt(editText_trybok_v_kose.getText().toString());
-                    }
-                    if (dlina_cosu>=240&dlina_cosu<360){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_4_240");
-                        dlina_blije_k_vbIbr_kose = 240*4/Integer.parseInt(editText_trybok_v_kose.getText().toString());
-                    }
-                    if (dlina_cosu>=360){
-                        cena_blije_k_vbIbr_kose = loadText("edit_text_cenakos_4_360");
-                        dlina_blije_k_vbIbr_kose = 360*4/Integer.parseInt(editText_trybok_v_kose.getText().toString());
-                    }
-
-                }
-
-
-
-
+                chislo = dlina_cosu*Integer.parseInt(editText_trybok_v_kose.getText().toString());
+                osnovanie = 5008;
+                ccc = (2.48178 - Math.log(chislo*100)/Math.log(osnovanie))*chislo;
+                String ggg = new DecimalFormat("#0.00").format(ccc) + " грн";
+                //TextView textView =  findViewById(R.id.textView_rezylbtat);
+                //textView.setText(ggg);
+                skidka_rezylbtat = ccc*(100-skidka)/100;
+                //String ggg1 = new DecimalFormat("#0.00").format(skidka_rezylbtat) + " грн";
+                //TextView textView_skidka =  findViewById(R.id.textView_skidka_rezylbtat);
+                //textView_skidka.setText(ggg1);
 
                 fff = Integer.parseInt(cena_blije_k_vbIbr_kose)*dlina_cosu/dlina_blije_k_vbIbr_kose;
                 //String ggg = Double.toString(fff) + " грн";
-                String ggg1 = Double.toString(dlina_cosu) + " см";
+                String ggg_dlina = Double.toString(dlina_cosu) + " см";
                 TextView textView1 =  findViewById(R.id.textView_rezylbtat_dlina);
-                textView1.setText(ggg1);
+                textView1.setText(ggg_dlina);
                 //String ggg1 = String.format("%2.f",skidka_rezylbtat) + " грн";
-                String ggg = new DecimalFormat("#0.00").format(fff) + " грн";
+                //String ggg = new DecimalFormat("#0.00").format(fff) + " грн";
                 TextView textView =  findViewById(R.id.textView_rezylbtat);
                 textView.setText(ggg);
                 skidka_rezylbtat = fff*(100-skidka)/100;
@@ -146,11 +91,11 @@ public class CenacosRound extends AppCompatActivity {
         });
     }
 
-    private String loadText(String nidname) {
-        sp = PreferenceManager.getDefaultSharedPreferences(this);
-        Parser_DATA = sp.getString(nidname, "");
-        return Parser_DATA;
-    }
+//    private String loadText(String nidname) {
+//        sp = PreferenceManager.getDefaultSharedPreferences(this);
+//        Parser_DATA = sp.getString(nidname, "");
+//        return Parser_DATA;
+//    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -166,9 +111,7 @@ public class CenacosRound extends AppCompatActivity {
         int id = item.getItemId();
 
         switch (id) {
-            case R.id.nastroiki:
-                nastroikifynk();
-                break;
+//            120
             case R.id.glavnaya:
                 mainactivityfynk();
                 break;
@@ -177,12 +120,12 @@ public class CenacosRound extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     //ЗАПУСК АКТИВИТИ НАСТРОЙКИ
-    private void nastroikifynk() {
+    /*private void nastroikifynk() {
         Intent intent = new Intent(this, PrefActivity.class);
         startActivity(intent);
         finish();
 
-    }
+    }*/
     private void mainactivityfynk() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
