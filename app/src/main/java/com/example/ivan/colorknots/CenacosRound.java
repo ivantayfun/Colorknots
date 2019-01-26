08941f51980d116314e7222e531ec26db47828de
Ivan Rybakov <ivan.rubakov85@gmail.com>
2019-01-26 09:53:47 +0200

    26.01.2019 работчая master

--- app/src/main/java/com/example/ivan/colorknots/CenacosRound.java
+++ app/src/main/java/com/example/ivan/colorknots/CenacosRound.java
@@ -60,29 +60,19 @@ public class CenacosRound extends AppCompatActivity {
                 } else {
                     dlina_cosu = Integer.parseInt(editText_shirina_ovala.getText().toString()) * 3.14 + (Integer.parseInt(editText_dlina_ovala.getText().toString())-Integer.parseInt(editText_shirina_ovala.getText().toString())) * 2;
                 }
-
+                //master
+                String ggg_dlina = Double.toString(dlina_cosu) + " см";
+                TextView textView1 =  findViewById(R.id.textView_rezylbtat_dlina);
+                textView1.setText(ggg_dlina);
 
                 chislo = dlina_cosu*Integer.parseInt(editText_trybok_v_kose.getText().toString());
                 osnovanie = 5008;
                 ccc = (2.48178 - Math.log(chislo*100)/Math.log(osnovanie))*chislo;
                 String ggg = new DecimalFormat("#0.00").format(ccc) + " грн";
-                //TextView textView =  findViewById(R.id.textView_rezylbtat);
-                //textView.setText(ggg);
-                skidka_rezylbtat = ccc*(100-skidka)/100;
-                //String ggg1 = new DecimalFormat("#0.00").format(skidka_rezylbtat) + " грн";
-                //TextView textView_skidka =  findViewById(R.id.textView_skidka_rezylbtat);
-                //textView_skidka.setText(ggg1);
-
-                fff = Integer.parseInt(cena_blije_k_vbIbr_kose)*dlina_cosu/dlina_blije_k_vbIbr_kose;
-                //String ggg = Double.toString(fff) + " грн";
-                String ggg_dlina = Double.toString(dlina_cosu) + " см";
-                TextView textView1 =  findViewById(R.id.textView_rezylbtat_dlina);
-                textView1.setText(ggg_dlina);
-                //String ggg1 = String.format("%2.f",skidka_rezylbtat) + " грн";
-                //String ggg = new DecimalFormat("#0.00").format(fff) + " грн";
                 TextView textView =  findViewById(R.id.textView_rezylbtat);
                 textView.setText(ggg);
-                skidka_rezylbtat = fff*(100-skidka)/100;
+
+                skidka_rezylbtat = ccc*(100-skidka)/100;
                 String ggg2 = new DecimalFormat("#0.00").format(skidka_rezylbtat) + " грн";
                 TextView textView_skidka =  findViewById(R.id.textView_skidka_rezylbtat);
                 textView_skidka.setText(ggg2);
